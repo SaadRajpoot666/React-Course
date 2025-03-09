51d3ecadfc8103af35fff2b575abb20009bd845f
SaadbKhalid <166914030+SaadRajpoot666@users.noreply.github.com>
2025-03-09 11:11:05 +0500

    Update TeamMember.jsx

--- Routing/pages/TeamMember.jsx
+++ Routing/pages/TeamMember.jsx
@@ -1,3 +1,13 @@
-export const  TeamMember = ({name})=>{
-return <h1>HI its {name}</h1>
-}
+ import { useParams } from "react-router-dom";
+import db from "../../db.json";
+
+export const TeamMember = () => {
+    const { memberId } = useParams();
+    const member = db.find((i) => i.id === memberId);
+
+    if (!member) {
+        return <h1>Member not found</h1>;
+    }
+
+    return <h1>Hi, it's {member.name}!</h1>;
+};
